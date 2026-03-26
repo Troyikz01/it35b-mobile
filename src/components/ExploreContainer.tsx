e2dd3113f7f0115a75ae9a352923820180c01aa7
troyikz01 <troyrojo80@gmail.com>
2026-03-26 17:48:57 +0800

    added password

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,13 @@
 import './ExploreContainer.css';
+import { IonInput, IonInputPasswordToggle } from '@ionic/react';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+    <IonInput type="password" label="Password" value="NeverGonnaGiveYouUp">
+      <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+    </IonInput>
   );
 };
 
